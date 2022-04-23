87d84aefc1849574e21d2ff3e23437f12b777a58
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-04-23 19:30:47 -0400

    Add files via upload

---
+++ week2/cocchiAmericano.js
@@ -0,0 +1,13 @@
+{
+    "ingredients": [
+        {
+            "idIngredient": "555",
+            "strIngredient": "Cocchi Americano",
+            "strDescription": "Cocchi Americano (Pronounced: ˈkɔkki ameriˈkano) is a quinine-laced aperitif wine produced by Giulio Cocchi Spumanti in the Asti province of Italy. Cocchi Americano is a variety of Americano. The wine was developed by Giulio Cocchi, and production began in 1891. Cocchi also produces a pink variety of this aperitif, the \"Cocchi Americano Rosa\", which is slightly more bitter and aromatic than the classic white Cocchi Americano.\r\n\r\n",
+            "strType": "Aperitif",
+            "strAlcohol": "Yes",
+            "strABV": null
+        }
+    ]
+}
+//link https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=555
