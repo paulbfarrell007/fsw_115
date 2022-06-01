9f5bdf59c61bbc1226eeceb1dfa736b6d5137699
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-06-01 11:48:57 -0400

    Add files via upload

--- week4/ToDoList/script.js
+++ week4/ToDoList/script.js
@@ -1,35 +1,35 @@
 axios.get("http://api.bryanuniversity.edu/paulfarrell/list")
     .then(response => {
-
+        let data = response.data
         for(let i = 0; i < response.data.length; i++) {
-
+            console.log(data[i].isComplete)
             const newDiv = document.createElement("div")
-                document.body.appendChild(newDiv)
             
-                const newP1 = document.createElement("p")
-                newP1.textContent = "Description: " + response.data[i].name
-                newDiv.appendChild(newP1)
+                
             
-                const newLabel = document.createElement("label")
-                newLabel.textContent = response.data[i].name
-                newDiv.appendChild(newLabel)             
+    const newP1 = document.createElement("p")
+    newP1.textContent = "Description: " + data[i].description
+    newDiv.appendChild(newP1)
             
-            const newInputBox = document.createElement("input")
-                newInputBox.setAttribute("type","checkbox")
-                newDiv.appendChild(newInputBox)               
+    const newLabel = document.createElement("label")
+    newLabel.textContent = data[i].name
+    newDiv.appendChild(newLabel)                         
+            
+    const newInputBox = document.createElement("input")
+    newInputBox.setAttribute("type","checkbox")
+    newInputBox.checked = data[i].isComplete
+    newDiv.appendChild(newInputBox)          
 
-            
+    const newP2 = document.createElement("p")
+    newP2.textContent = "Price: " + data[i].price
+    newDiv.appendChild(newP2)
 
-            const newP2 = document.createElement("p")
-                newP2.textContent = "Price: " + response.data[i].name
-                newDiv.appendChild(newP2)
-
-        if (response.data[i].completed === true) {
-                newLabel.style.textDecoration = 'line-through'            
+    if (data[i].isComplete === true) {
+        newLabel.style.textDecoration = 'line-through'            
             }
-              
+        document.body.appendChild(newDiv) 
         }
     })   
     
-.catch(error => console.log(error))
+.catch()
  
